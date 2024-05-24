2f0b13e3c8fdd96f5dd2cbada35e673fdba60d57
stvjc <stvjc@channing.harvard.edu>
2024-05-24 14:54:30 -0400

    first draft

--- basic.R
+++ basic.R
@@ -4,9 +4,18 @@ library(dplyr)
 
 setClass("ggiResource", slots=c(start="numeric",
  end="numeric", seqnames="ANY", space="ANY",
- score="numeric"))
+ score="numeric", tbl="ANY"))
+
+setMethod("show", "ggiResource", function(object) {
+ callNextMethod()
+})
 
 setClass("ABRIGresource", contains="ggiResource")
+
+setMethod("show", "ABRIGresource", function(object) {
+ cat("CDNM ggiResource\n")
+ print(slot(object, "tbl"))
+})
 
 library(EnsDb.Hsapiens.v75)
 
@@ -19,11 +28,25 @@ names(pfiles) = c("BAL", "BroncEpiBrush", "CD4Stim", "CD4Unstim",
                   "AlvMacphage", "PaxRNA")
 
 
-gloc_hg19 = genes(EnsDb.Hsapiens.v75)
+if (!exists("gloc_hg19")) gloc_hg19 = genes(EnsDb.Hsapiens.v75)
 
 ABRIGresource = function(con, tissue) {
    ttypes = c("BAL", "BroncEpiBrush", "CD4Stim", "CD4Unstim",
                   "AlvMacphage", "PaxRNA")
    stopifnot(tissue %in% ttypes)
-   dplyr::tbl(con, pfiles[tissue])
+   ans = dplyr::tbl(con, pfiles[tissue])
+   new("ABRIGresource", tbl=ans)
 }
+
+filterByHGNC = function(res, ggr, sym, radius=1e5, field="gene_name") {
+  stopifnot(inherits(res, "ABRIGresource"))
+  ok = ggr[which(mcols(ggr)[[field]] == sym)]
+  stopifnot(width(ok)>0)
+  anac = function(x) as.numeric(as.character(x)) # for Rle
+  ans = slot(res, "tbl") |> dplyr::filter(CHR == local(anac(seqnames(ok)[1])), BP >= IRanges::start(ok)-radius, BP<= IRanges::end(ok)+radius)
+  new("ABRIGresource", tbl=ans)
+}
+
+kk <- filterByHGNC(ll, gloc_hg19, "DSP")
+slot(kk, "tbl") |> group_by(gene,CHR,BP,SNP) |> summarise(n=n())
+
