92f04bd7b9166cbd19014380480d771150555423
stvjc <stvjc@channing.harvard.edu>
2024-07-08 09:14:29 -0400

    adding tab demo

--- GGIpack/R/tinyapp.R
+++ GGIpack/R/tinyapp.R
@@ -36,3 +36,51 @@ tinyapp = function(con, genelocs) {
  
  runApp(list(ui=ui, server=server))
 }
+
+#' demo app 2
+#' @import shiny
+#' @param con a DBI connection
+#' @param genelocs a GRanges instance with gene addresses
+#' @note Very specialized, just has a few genes, uses specific
+#' field from genelocs argument.
+#' @examples
+#' gloc_hg19 = ensembldb::genes(EnsDb.Hsapiens.v75::EnsDb.Hsapiens.v75)
+#' con = DBI::dbConnect(duckdb::duckdb())
+#' tinyapp2(con, gloc_hg19)
+#' @export
+tinyapp2 = function(con, genelocs) {
+ pfiles <<- ABRIGparquet_paths()
+ ui = fluidPage(
+  sidebarLayout(
+   sidebarPanel(
+    helpText("GGI demo"),
+helpText("get rid of radio buttons!"),
+    radioButtons("tiss", "tissue", names(pfiles)),
+    selectInput("gene", "gene", c("ORMDL3", "DSP", "AGL", "DBT", "SASS6"))
+    ), 
+   mainPanel(
+    #verbatimTextOutput("stuff")
+    uiOutput("alltabs")
+    )
+  )
+ )   # also need tabs, about etc.
+ 
+ server = function(input, output) {
+  output$stuff = renderPrint({
+   mygene = input$gene
+   mytiss = input$tiss
+   newres = ABRIGresource(con, input$tiss)
+   kk <- filterByRange(newres, genelocs, mygene, ggr_field="gene_name")
+   kk@tbl
+  })
+  output$alltabs = renderUI({
+   tabsetPanel(
+    tabPanel("BAL", helpText("A"), verbatimTextOutput("stuff")),
+    tabPanel("BronchEpiBrush", helpText("A")),
+    tabPanel("CD4stim", helpText("A"))
+    )
+   })
+ }
+ 
+ runApp(list(ui=ui, server=server))
+}
