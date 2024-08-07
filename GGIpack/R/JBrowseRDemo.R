00425a8e0e3767e3e1395697a8ef3979e9b174ee
Sara Stankiewicz <sarastankiewicz@pc002425.partners.org>
2024-08-07 09:39:49 -0400

    adding changes before meeting with John

--- GGIpack/R/JBrowseRDemo.R
+++ GGIpack/R/JBrowseRDemo.R
@@ -18,12 +18,22 @@ server <- function(input, output, session) {
     refname_aliases = "https://s3.amazonaws.com/jbrowse.org/genomes/hg19/hg19_aliases.txt"
   )
   
+ #df <- data.frame(
+  #chrom = c('1', '2'),
+   # start = c(123, 456),
+    #end = c(789, 101112),
+    #name = c('feature1', 'feature2')
+  #)
+  
+  path = system.file("extdata/Alveolar_Macrophages_IS.MICA:ILMN_3241692.CAU.meta", package="GGIpack")
+  df_origin = utils::read.table(path, header = TRUE)
   df <- data.frame(
-    chrom = c('1', '2'),
-    start = c(123, 456),
-    end = c(789, 101112),
-    name = c('feature1', 'feature2')
+    chrom =df_origin$CHR ,
+    start =df_origin$BP,
+    end = df_origin$BP,
+    name = df_origin$SNP
   )
+  
   
   df_track <- JBrowseR::track_data_frame(df, "foo", assembly)
   
